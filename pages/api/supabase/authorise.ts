b01816230d79305ce67b7df126c12414be4cd391
Alex Sherwood <aplssf@users.noreply.github.com>
2023-05-18 15:20:35 +0100

    Add SERVER_BASE_URL

--- pages/api/supabase/authorise.ts
+++ pages/api/supabase/authorise.ts
@@ -21,7 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // !! Needs to be updated to use S variables
   const N_ALLOWED_REDIRECT_URIS = process.env.N_REDIRECT_URIS!.split(', ');
   const proxyRedirectUri = (typeof query.redirect_uri === 'string' && N_ALLOWED_REDIRECT_URIS.includes(query.redirect_uri.toString()))
-    ? process.env.S_PROXY_REDIRECT_URL
+    ? process.env.SERVER_BASE_URL + process.env.S_PROXY_REDIRECT_URL
     : 'null';
 
   const supabase = createPagesServerClient(
